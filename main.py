fb0e199deae55b28218293aa249c40eeba05bcd0
mado4335@gmail.com <aA52978130>
2023-02-08 16:01:46 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+import torch
+import cv2
+
+
+def camera_start():
+    return cv2.VideoCapture(0)
+
+
+def load_model():
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+    return model
+
+
+def predict_labels(frame):
+    model.to(device)
+    frame = [frame]
+    results = model(frame)
+    labels, cord = results.xyxyn[0][:, -1].numpy(), results.xyxyn[0][:, :-1].numpy()
+    return labels, cord
+
+
+def get_labels(x):
+    return classes[int(x)]
+
+
+def plot_labels(results, frame):
+    labels, cord = results
+    n = len(labels)
+    x_shape, y_shape = frame.shape[1], frame.shape[0]
+    for i in range(n):
+        row = cord[i]
+        if row[4] >= 0.2:
+            x1, y1, x2, y2 = int(row[0] * x_shape), int(row[1] * y_shape), int(row[2] * x_shape), int(row[3] * y_shape)
+            bgr = (0, 255, 0)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)
+            cv2.putText(frame, get_labels(labels[i]), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)
+    return frame
+
+
+model = load_model()
+classes = model.names
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+player = camera_start()
+
+while True:
+    ret, frame = player.read()
+    results = predict_labels(frame)
+    frame = plot_labels(results, frame)
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
